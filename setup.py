8c4e7e31476228a6e54064abab4db1fe6656bd48
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2025-05-19 01:02:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ dev_requires = [
 
 setup(
     name="projectx-sdk",
-    version="0.2.2",
+    version="0.2.3",
     author="Christian Starr",
     author_email="christianjstarr@icloud.com",
     description="UnofficialPython SDK for the ProjectX Gateway API",
